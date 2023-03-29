b209b32ebdfeed1cb4ddccf090878ccb8a177317
Tristan Gruener <tristan.gruener@gmail.com>
2023-03-29 15:10:52 -0700

    pulling in weather data

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { city: 'Monterey', error: null });
+  res.render("index", { weatherOne: {}, error: null });
 });
 
 app.get("/forecast", async (req, res) => {
 
   const city = req.query.city;
+  const apiKey = '3aa725c3067843f795c99d04f5c43586'
 
-  res.render("index", { city: city, error: null });
+  fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=Bend&units=I&days=7&key=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherOne = data.data[0]
+      console.log(weatherOne)
+      res.render("index", { weatherOne: weatherOne, error: null });
+    });
 });
 
 const port = process.env.PORT || 3000;
