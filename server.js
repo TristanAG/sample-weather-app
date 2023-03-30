517150f49ce618f86b040a8489af77039d0622fa
Tristan Gruener <tristan.gruener@gmail.com>
2023-03-30 10:27:02 -0700

    extract forecast object creation into its own function

--- server.js
+++ server.js
@@ -18,37 +18,42 @@ app.get("/forecast", async (req, res) => {
     .then((response) => response.json())
     .then((data) => {
 
-      // set day function now
+      const weatherForecastData = setForecastData(data.data)
 
-      const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-      const dayOfWeekOrdered = []
-      
-      // today === 4
-      const today = new Date().getDay()
-      
-      let j = 0
-      for (i = 0; i < data.data.length; i++) {
-        if (today + i <= 6) {
-          dayOfWeekOrdered.push(dayOfWeek[today + i])
-        } else {
-          dayOfWeekOrdered.push(dayOfWeek[j])
-          j++
-        }
-      }
-      
-      const weather = [
-        {day: dayOfWeekOrdered[0], ...data.data[0]}, 
-        {day: dayOfWeekOrdered[1], ...data.data[1]}, 
-        {day: dayOfWeekOrdered[2], ...data.data[2]},
-        {day: dayOfWeekOrdered[3], ...data.data[3]},
-        {day: dayOfWeekOrdered[4], ...data.data[4]},
-        {day: dayOfWeekOrdered[5], ...data.data[5]},
-        {day: dayOfWeekOrdered[6], ...data.data[6]},
-      ]
-
-      res.render("index", { weather: weather, error: null });
+      res.render("index", { weather: weatherForecastData, error: null });
     });
 });
+
+const setForecastData = (weekData) => {
+  
+  // days of week aren't available in api, so need to manually set them to weather forecast array
+  const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  const dayOfWeekOrdered = []
+  
+  const today = new Date().getDay()
+  
+  let j = 0
+  for (i = 0; i < weekData.length; i++) {
+    if (today + i <= 6) {
+      dayOfWeekOrdered.push(dayOfWeek[today + i])
+    } else {
+      dayOfWeekOrdered.push(dayOfWeek[j])
+      j++
+    }
+  }
+  
+  const weatherForecast = [
+    {day: dayOfWeekOrdered[0], ...weekData[0]}, 
+    {day: dayOfWeekOrdered[1], ...weekData[1]}, 
+    {day: dayOfWeekOrdered[2], ...weekData[2]},
+    {day: dayOfWeekOrdered[3], ...weekData[3]},
+    {day: dayOfWeekOrdered[4], ...weekData[4]},
+    {day: dayOfWeekOrdered[5], ...weekData[5]},
+    {day: dayOfWeekOrdered[6], ...weekData[6]},
+  ]
+
+  return weatherForecast
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
