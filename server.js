798f327f13807dbd7798c3da1e877caacbd3dea9
Tristan Gruener <tristan.gruener@gmail.com>
2023-03-30 10:20:01 -0700

    some style and rendering days of the week

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.static("style"));
 
 app.get("/", (req, res) => {
-  res.render("index", { weather: [], weatherOne: {}, weatherTwo: {}, error: null });
+  res.render("index", { weather: [], error: null });
 });
 
 app.get("/forecast", async (req, res) => {
@@ -17,13 +17,36 @@ app.get("/forecast", async (req, res) => {
   fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=Bend&units=I&days=7&key=${apiKey}`)
     .then((response) => response.json())
     .then((data) => {
-      const weatherOne = data.data[0]
-      const weatherTwo = data.data[1]
 
-      const weather = [data.data[0], data.data[1], data.data[2], data.data[3], data.data[4], data.data[5], data.data[6]]
+      // set day function now
 
-      console.log(weatherOne)
-      res.render("index", { weather: weather, weatherOne: weatherOne, weatherTwo: weatherTwo, error: null });
+      const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+      const dayOfWeekOrdered = []
+      
+      // today === 4
+      const today = new Date().getDay()
+      
+      let j = 0
+      for (i = 0; i < data.data.length; i++) {
+        if (today + i <= 6) {
+          dayOfWeekOrdered.push(dayOfWeek[today + i])
+        } else {
+          dayOfWeekOrdered.push(dayOfWeek[j])
+          j++
+        }
+      }
+      
+      const weather = [
+        {day: dayOfWeekOrdered[0], ...data.data[0]}, 
+        {day: dayOfWeekOrdered[1], ...data.data[1]}, 
+        {day: dayOfWeekOrdered[2], ...data.data[2]},
+        {day: dayOfWeekOrdered[3], ...data.data[3]},
+        {day: dayOfWeekOrdered[4], ...data.data[4]},
+        {day: dayOfWeekOrdered[5], ...data.data[5]},
+        {day: dayOfWeekOrdered[6], ...data.data[6]},
+      ]
+
+      res.render("index", { weather: weather, error: null });
     });
 });
 
